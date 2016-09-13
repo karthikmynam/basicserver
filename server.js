12546dd40a86b50520e35f8f95cf90e8c6d0b8eb
Karthik Mynam <krthkmyn@gmail.com>
2016-09-12 21:52:17 -0400

    Basic server

---
+++ server.js
@@ -0,0 +1,18 @@
+
+var IP_ADDRESS = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var PORT = ( process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT ) || 3500;
+
+var express = require("express");
+var app = express();
+
+
+app.use('/', function(req, res){
+
+	console.log("Received request");
+	res.send("Hello World!");
+});
+
+app.listen(PORT, IP_ADDRESS, function() {
+
+	console.log("Server started on port: " + IP_ADDRESS + ":" +  PORT);				
+});
